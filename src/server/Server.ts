d809a4774f41d08a3bb4cbbdbbede0075319defb
Felipe Menegueli <felipemenegueli08@gmail.com>
2023-05-31 17:01:24 -0300

    Ajustando o eslint para codificação

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,9 @@
-import express from 'express';
+import express from 'express'
 
-const server = express();
+const server = express()
 
 server.get('/', (_, res) => {
-
   return res.send('Hello')
 })
 
-export { server };
+export { server }
